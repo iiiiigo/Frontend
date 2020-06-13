42e86934c4c9393a761d3eb1f5a38d802246eae9
inho2736 <limcara@nate.com>
2020-06-13 20:46:06 +0900

    main페이지 수정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,48 +1,109 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import menu_bar from './icon/menubar_hamburger.png';
-import close_bar from './icon/menubar_close.png';
-import naver_logo from './icon/NAVER_D2_logo.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import menu_bar from "./icon/menubar_hamburger.png";
+import close_bar from "./icon/menubar_close.png";
+import logo from "./icon/igo.JPG";
 
-import './component.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from "reactstrap";
 
-export default function Layout({children}) {
-    const handleClickMenu = () => {
-        const x = document.getElementById('menu-img') || document.getElementById('close-img');
-        const y = document.getElementById('open-button') || document.getElementById('close-button');
-        if(x.src === menu_bar){
-            x.src = close_bar;
-            x.id = 'close-img'
-            y.id = 'close-button';
-        }else{
-            x.src = menu_bar;
-            x.id = 'menu-img'
-            y.id = 'open-button';
-        }
+import "./component.css";
+
+export default function Layout({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const handleClickMenu = () => {
+    const x =
+      document.getElementById("menu-img") ||
+      document.getElementById("close-img");
+    const y =
+      document.getElementById("open-button") ||
+      document.getElementById("close-button");
+    if (x.src === menu_bar) {
+      x.src = close_bar;
+      x.id = "close-img";
+      y.id = "close-button";
+    } else {
+      x.src = menu_bar;
+      x.id = "menu-img";
+      y.id = "open-button";
     }
+  };
   return (
     <span>
-        <button type='button' id='open-button' onClick={handleClickMenu}>
-            <img src={menu_bar} alt="menubar" id='menu-img'/>
-        </button>
-        <div id="link-div">
-            <Link to={{pathname:'/'}} id='link'>
-                <div id='naver_logo'>
-                    <strong>다람쥐 컴퍼니</strong>
-                </div>
-                {/* <img src={naver_logo} alt="" id='naver_logo'/> */}
-            </Link>
-        </div>
-        <ul id='menu-bar'>
-            <li><Link to={{pathname:'/'}}>직원 리스트</Link></li>
-            <li><Link to={{pathname:'/create'}}>신규 등록하기</Link></li>
-        </ul>
-        <body>
-            {children}
-        </body>
-        <footer>
-          <p>Copyright © NAVER Corp. All Rights Reserved.</p>
-        </footer>
+      <Navbar color="" light expand="md">
+        <NavbarBrand href="/">
+          Posture Correcting Web App
+          <img src={logo} alt="menubar" id="menu-img" width="100" />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">내 자세 진단하기</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                동영상 카테고리
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+
+      {/* <button type="button" id="open-button" onClick={handleClickMenu}>
+        <img src={menu_bar} alt="menubar" id="menu-img" />
+      </button>
+      <div id="link-div">
+        <Link to={{ pathname: "/" }} id="link">
+          <div id="naver_logo">
+            <strong>다람쥐 컴퍼니</strong>
+          </div>
+         
+        </Link>
+      </div>
+      <ul id="menu-bar">
+        <li>
+          <Link to={{ pathname: "/" }}>직원 리스트</Link>
+        </li>
+        <li>
+          <Link to={{ pathname: "/create" }}>신규 등록하기</Link>
+        </li>
+      </ul> */}
+      <body>{children}</body>
+      <footer>
+        <p>Copyright © Igo Corp. All Rights Reserved.</p>
+        <p> Team I go </p>
+        <p>
+          Address : 서울특별시 동작구 상도동 상도로 369 숭실대학교 정보과학관
+        </p>
+      </footer>
     </span>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,48 +1,151 @@
-import React, { useEffect, useCallback } from 'react';
-import { Layout, Loading } from '../components';
-import { useHistory, useLocation, Link } from 'react-router-dom';
-import chipmunk from '../components/icon/chipmunk.jpeg';
-import { useEmployeeList } from '../hooks';
-import './page.css';
+import React, { useEffect, useCallback, useState } from "react";
+import { Layout, Loading } from "../components";
+import { useHistory, useLocation, Link } from "react-router-dom";
+import kakaoRunning from "../components/icon/kakao_running.png";
+import { useEmployeeList } from "../hooks";
+import logo from "../components/icon/igo.JPG";
+import yoga from "../components/icon/yoga.jpg";
+import neck from "../components/icon/forwardNeck.JPG";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from "reactstrap";
+import "./page.css";
 
 export default function MainPage() {
   const history = useHistory();
-  const [{employee, isLoading}, startLoading, endLoading] = useEmployeeList();
-  const handleClickEmployee = (value) => {
+  const [{ employee, isLoading }, startLoading, endLoading] = useEmployeeList();
+  const handleClickEmployee = value => {
     startLoading();
     history.push(`/update`, value); // api 미작동
     endLoading();
   };
-  
+
+  const items = [
+    {
+      src: neck,
+      altText: "당신도 혹시 거북목?",
+      caption: "<내 자세 진단하기>를 통해 거북목 증상을 실시간으로 확인하세요.",
+      header: "Slide 1 Header",
+      key: "1"
+    },
+    {
+      src: yoga,
+      altText: "자세교정을 통한 몸매 가꾸기",
+      caption: "모션인식으로 바른자세를 쉽고 정확하게, 지금 시작하세요!",
+      header: "Slide 2 Header",
+      key: "2"
+    },
+    {
+      src: kakaoRunning,
+      altText: "카카오 프렌즈와 함께 달려볼까?",
+      caption:
+        "나이키 조이라이드 x 카카오프렌즈 콜라보 한정판 컬렉션 지금보러가기",
+      header: "Slide 3 Header",
+      key: "3"
+    }
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = newIndex => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map(item => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img
+          className="d-block w-100"
+          src={item.src}
+          alt={item.altText}
+          style={{ display: "block", filter: "brightness(50%)" }}
+        />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.altText}
+        />
+      </CarouselItem>
+    );
+  });
   return (
     <Layout>
-      <div style={{backgroundColor: 'white', alignItems: 'center'}}>
-        <img src={chipmunk} alt='다람쥐' style={{  display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '50%'}}/>
-      </div>
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
+      {/* <div style={{ backgroundColor: "white", alignItems: "center" }}>
+        <img
+          src={kakaoRunning}
+          alt="kakao x nike 콜라보 이미지"
+          style={{
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%"
+          }}
+        />
+      </div> */}
+      {/*       
       <div>
         <strong>회사원 목록</strong>
       </div>
-      <div id='div_list'>
-        <ul  id='employee'>
+      <div id="div_list">
+        <ul id="employee">
           {employee.map(value => {
             return (
-              <div key={value.id} id="list_li" onClick={()=>handleClickEmployee(value)}>
-                  <li>
+              <div
+                key={value.id}
+                id="list_li"
+                onClick={() => handleClickEmployee(value)}
+              >
+                <li>
                   name: <strong>{value.employee_name}</strong>
-                  </li>
-                  <li>
-                  age: {value.employee_age}
-                  </li>
-                  <li>
-                  salary : {value.employee_salary}
-                  </li>
+                </li>
+                <li>age: {value.employee_age}</li>
+                <li>salary : {value.employee_salary}</li>
               </div>
-            )
+            );
           })}
         </ul>
-      </div>
+        </div>
 
-      <Loading open={isLoading} />
+      <Loading open={isLoading} />*/}
     </Layout>
   );
 }
